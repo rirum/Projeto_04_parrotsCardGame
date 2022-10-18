2941c3b77f0c719b3825afbeda566a327f3922d1
camille <c.miyashita@gmail.com>
2022-10-18 00:55:02 -0300

    card inicial

---
+++ script.js
@@ -0,0 +1,12 @@
+//- [ ]  Ao entrar no jogo, o usuário deverá ser perguntado com quantas cartas quer jogar (utilize `prompt`)
+
+
+
+
+
+/** O usuário só poderá inserir números pares no prompt, de 4 a 14. 
+ * Qualquer número que fuja a essa regra não deve ser aceito. No caso de números inválidos, o prompt deverá ficar sendo repetido, até que o usuário coloque um número válido.
+ */
+
+
+/**Após inserir um número de cartas válido, o jogo deverá inserir as cartas viradas pra baixo na página de forma que a distribuição seja aleatória*/'
