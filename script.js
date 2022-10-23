ca9e2400190f187204fe79aa3d215035ccd69aae
camille <c.miyashita@gmail.com>
2022-10-23 16:50:37 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-//- [ ]  Ao entrar no jogo, o usuário deverá ser perguntado com quantas cartas quer jogar (utilize `prompt`)
-/** O usuário só poderá inserir números pares no prompt, de 4 a 14. 
- * Qualquer número que fuja a essa regra não deve ser aceito. No caso de números inválidos, o prompt deverá ficar sendo repetido, até que o usuário coloque um número válido.
- */
-
 var quantasCartas = 0;
 let cartasSelecionadas = [];
+
+let primeiraCartaSelecionada = '';
+let segundaCartaSelecionada = '';
+
+let quantidadeCliquesCartas = 0;
+let quantidadeAcertos = 0;
 
 const imgsDeck = [
     "bobrossparrot.gif",
@@ -38,7 +39,7 @@ let cardHTML = "";
 cartasSelecionadas.sort(comparador);
 for (let i = 0; i < cartasSelecionadas.length; i++) {
     cardHTML += `
-    <div class="card card-${cartasSelecionadas[i]}">
+    <div class="card" onclick="clicarCarta(this)" id="${cartasSelecionadas[i]}">
     <div class="face back"> <img src="images/${cartasSelecionadas[i]}"></div>
     <div class="face front"> <img src="images/back2.png"> </div>
     </div>
@@ -49,24 +50,44 @@ cardBoard.innerHTML = cardHTML;
 
 
 
-/**function virarCarta(){
+function clicarCarta(selecionada) {
+    if (selecionada.className.includes('revelarCarta')) {
+        return;
+    }
 
-    let cartasClicadas
-} */
+    quantidadeCliquesCartas++;
+    if (primeiraCartaSelecionada === '' && segundaCartaSelecionada === '') {
+        primeiraCartaSelecionada = selecionada;
 
+        selecionada.classList.add('revelarCarta');
+    } else if (primeiraCartaSelecionada != '' && segundaCartaSelecionada === '') {
+        segundaCartaSelecionada = selecionada;
+        selecionada.classList.add('revelarCarta');
+        checarCartas(selecionada);
+    }
+}
 
+function checarCartas() {
+    const idPrimeiraCarta = primeiraCartaSelecionada.getAttribute('id');
+    const idSegundaCarta = segundaCartaSelecionada.getAttribute('id');
 
-/**- Clique na carta
-    - [ ]  Ao clicar em uma carta, a mesma deve ser virada
-    - [ ]  Caso seja a primeira carta do par, ela deve permanecer virada até o usuário escolher a segunda carta
-    - [ ]  Caso seja a segunda carta virada, existem 2 situações:
-        - [ ]  Caso seja igual à primeira carta, o usuário acertou e ambas agora devem ficar viradas pra cima até o final do jogo
-        - [ ]  Caso seja uma carta diferente da primeira carta virada, o usuário errou. Nesse caso, o jogo deve **aguardar 1 segundo** e então virar as duas cartas para baixo novamente
-    - [ ]  **Dica**: se quiser fazer o efeito 3D da carta virando, você pode ver o CSS necesśario em: [https://repl.it/@BootcampRespond/FlippedCards#index.html](https://repl.it/@BootcampRespond/FlippedCards#index.html) . Só se atente que nesse exemplo a carta vira ao passar o mouse. No caso do jogo, seria ao clicar.
-- Fim do Jogo
-    - [ ]  Quando o usuário terminar de virar todas as cartas corretamente, **deverá** ser exibido um `alert` com a mensagem **"Você ganhou em X jogadas!"** sendo X **a quantidade de vezes que o usuário virou uma carta no jogo**.
-        
-        Ou seja: **cada clique é uma jogada**. Não é virar duas cartas que é uma jogada.
-        
+    if (idPrimeiraCarta === idSegundaCarta) {
+        primeiraCartaSelecionada = '';
+        segundaCartaSelecionada = '';
+        quantidadeAcertos++;
 
-# Bônus (opcional) */
+        if (quantidadeAcertos * 2 === quantasCartas) {
+            setTimeout(() => {
+                    alert('Você ganhou em ' + quantidadeCliquesCartas + ' jogadas!');
+                },500);
+        }
+    } else {
+        setTimeout(() => {
+                primeiraCartaSelecionada.classList.remove('revelarCarta');
+                segundaCartaSelecionada.classList.remove('revelarCarta');
+
+                primeiraCartaSelecionada = '';
+                segundaCartaSelecionada = '';
+            },1000);
+    }
+}
